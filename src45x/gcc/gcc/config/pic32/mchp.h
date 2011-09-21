251b09b237a4efb86cfd1c00779edd4bd9ba8564
Jason Kajita <jason@jasonkajita.com>
2011-09-21 16:46:30 -0700

    Prepare for calling newlib

--- src45x/gcc/gcc/config/pic32/mchp.h
+++ src45x/gcc/gcc/config/pic32/mchp.h
@@ -68,7 +68,7 @@ do {                     \
  */
 #undef  LIB_SPEC
 #if 1 /* chipKIT */
-#define LIB_SPEC "--start-group -lc -lm -lsupc++ -lpic32 %{!mno-peripheral-libs:-lmchp_peripheral %{mprocessor=*:-lmchp_peripheral_%*}} --end-group"
+#define LIB_SPEC "--start-group -lc -lsupc++ -lpic32 -lgcc %{!mno-peripheral-libs:-lmchp_peripheral %{mprocessor=*:-lmchp_peripheral_%*}} --end-group"
 #else
 #define LIB_SPEC "--start-group %{!mno-mpdebug-lib:-ldebug} -lc -lm -le -ldsp -lsupc++ %{!mno-peripheral-libs:-lmchp_peripheral %{mprocessor=*:-lmchp_peripheral_%*}} --end-group"
 #endif
