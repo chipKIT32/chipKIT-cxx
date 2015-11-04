44fa78831496acf662929cd6e9c54d656fd24fb2
Jason Kajita <jason.kajita@microchip.com>
2015-11-03 17:49:18 -0700

    Remove old source files

--- src45x/binutils/bfd/version.h
+++
@@ -1,4 +0,0 @@
-#define BFD_VERSION_DATE   chipKIT Compiler for PIC32 MCUs v1.30.02-20110618
-#define BFD_VERSION @bfd_version@
-#define BFD_VERSION_STRING  @bfd_version_package@ @bfd_version_string@
-#define REPORT_BUGS_TO @report_bugs_to@
